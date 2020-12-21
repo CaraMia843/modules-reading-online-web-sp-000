1db0de5468b786c7219bd2485bc0b3eff8f283de
Cara Zellner <carajeanine@gmail.com>
2020-12-21 21:35:11 +0000

    Automatically backed up by Learn

--- lib/class_methods_module.rb
+++ lib/class_methods_module.rb
@@ -3,5 +3,13 @@ module MetaDancing
   def metadata
     "This class produces objects the love to dance."
   end
+  
+  class Dancer 
+    extend MetaDancing
+  end
+  
+  class Kid 
+    extend MetaDancing
+  end
 
 end
