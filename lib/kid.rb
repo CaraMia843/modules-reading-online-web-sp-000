c212e851e578e798ab8997b30d788842212b14b2
Cara Zellner <carajeanine@gmail.com>
2020-12-21 21:44:51 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -4,7 +4,7 @@ require_relative './fancy_dance.rb'
 
 class Kid 
   
-  extend FancyDace::ClassMethods 
+  extend FancyDance::ClassMethods 
   include FancyDance::InstanceMethods
   
   attr_accessor :name
