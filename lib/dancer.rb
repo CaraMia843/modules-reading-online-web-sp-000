0e30a9afdb9e237457e55dbf3652e1d41f7757da
Cara Zellner <carajeanine@gmail.com>
2020-12-21 21:43:02 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -3,8 +3,8 @@ require_relative './dance_module.rb'
 
 class Dancer 
   
-  include Dance 
-  extend MetaDancing
+  extend FancyDace::ClassMethods 
+  include FancyDance::InstanceMethods
   
   attr_accessor :name 
   
